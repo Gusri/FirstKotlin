c4f6c76f2c92ee9853e7becb6b55a59111d8514d
Gusri <ghostonk82@gmail.com>
2017-10-25 04:38:12 +0700

    first Kotlin using AS 3

---
+++ app/src/main/java/com/gus/firstkotlinproject/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.gus.firstkotlinproject
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        sendButton.setOnClickListener({
+            var inputName = nameText.text
+            PreviewName.text = ("Hallo " +inputName+ " Thanks For Test My App!")
+        })
+    }
+}
